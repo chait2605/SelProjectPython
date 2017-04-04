0f9f97a2a86520998f0e2d2d77ee8eeb2aa99b6c
chait2605 <chait.2605@gmail.com>
2017-04-04 02:16:32 -0700

    Modified file for search bar validation

--- searchproducts.py
+++ searchproducts.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 #create a new firefox gecko driver session
 driver = webdriver.Firefox(executable_path=r'C:\Users\SRIKUCHAITU\PycharmProjects\PracticeSelProject\geckodriver.exe')
@@ -7,6 +8,11 @@ driver.maximize_window()
 #navigate to the application
 driver.get("http://www.yahoo.com")
 assert 'Yahoo' in driver.title
+
+elem = driver.find_element_by_name('p')
+elem.send_keys('seleniumhq' + Keys.RETURN)
+
+driver.refresh()
 
 driver.close()
 driver.quit()
